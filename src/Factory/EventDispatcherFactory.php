4b6a0f55435a1bcd977293a91a7e9bee580005c3
iampersistent <shank@zestic.com>
2018-05-05 01:52:38 -0700

    update config layout for events

--- src/Factory/EventDispatcherFactory.php
+++ src/Factory/EventDispatcherFactory.php
@@ -13,9 +13,12 @@ class EventDispatcherFactory
         $config = $container->get('config');
         $events = $config['eventListener'];
         $dispatcher = new EventDispatcher();
-        foreach ($events as $event => $listeners) {
-            foreach ($listeners as $listener => $method) {
-                $dispatcher->addListener($event, [$container->get($listener), $method]);
+        foreach ($events as $eventClass => $classEvents) {
+            foreach ($classEvents as $step => $listeners) {
+                $eventName = $eventClass . '.' . $step;
+                foreach ($listeners as $listener => $method) {
+                    $dispatcher->addListener($eventName, [$container->get($listener), $method]);
+                }
             }
         }
         return $dispatcher;
