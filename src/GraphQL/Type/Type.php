aac50c00b9a87eeaee33965a42da532d6de0d268
iampersistent <shank@zestic.com>
2021-09-08 09:55:20 -0700

    fix paginate input in type

--- src/GraphQL/Type/Type.php
+++ src/GraphQL/Type/Type.php
@@ -3,21 +3,22 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Type;
 
+use App\GraphQL\Type\Input\PaginateInput;
 use App\GraphQL\Type\Object\PaginationObject;
 use GraphQL\Type\Definition\Type as BaseType;
 
 abstract class Type extends BaseType
 {
-    private static $paginationInput;
+    private static $paginateInput;
     private static $paginationObject;
+
+    public static function paginationInput()
+    {
+        return self::$paginateInput ?: (self::$paginateInput = new PaginateInput());
+    }
 
     public static function paginationObject()
     {
         return self::$paginationObject ?: (self::$paginationObject = new PaginationObject());
-    }
-
-    public static function paginationInput()
-    {
-        return self::$paginationInput ?: (self::$paginationInput = new PaginationObject());
     }
 }
